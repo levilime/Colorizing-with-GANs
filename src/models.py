01a3ea091be7785d2aca92584e1ac2551b2ace6e
Levi <levilime@users.noreply.github.com>
2018-06-13 16:42:06 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -124,7 +124,7 @@ class BaseModel:
                 step = step + 1
 
         result = np.mean(np.array(result), axis=0)
-        if len(result > 4):
+        if len(result) > 4:
             print('Results: D loss: %f - D fake: %f - D real: %f - G loss: %f - G L1: %f - G gan: %f - accuracy: %f'
                   % (result[0] + result[1], result[0], result[1], result[2] + result[3], result[2], result[3], result[4]))
         else:
